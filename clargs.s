79b7ff4cc4511ed0960be67e3bfea1a53f2d6d76
Denver Coneybeare <dconeybeare@rim.com>
2011-07-05 15:27:09 -0400

    clargs.s: git it working; the ecx register was being clobbered by printf

--- clargs.s
+++ clargs.s
@@ -1,24 +1,50 @@
     .data
 template:
-    .ascii "%s\n\0"
+    .ascii "%i: %s\n\0"
 
     .text
 .global go
 go:
-    enter $8, $0
-    movl 8(%ebp), %ecx
-    movl 12(%ebp), %esi
+    # reserve 4 bytes on the stack for the local variable "i"
+    enter $4, $0
+
+    # Local Variables:
+    #   argc: 8(%ebp)
+    #   argv: 12(%ebp)
+    #   i: -4(%ebp)
+
+    # initialize i to 0
+    movl $0, -4(%ebp)
 
 argloop:
-    pushl (%esi)
+
+    # copy i into the ecx register
+    movl -4(%ebp), %ecx
+
+    # test if i>=argc, and if it is, exit the loop
+    cmpl 8(%ebp), %ecx
+    jge argloopend
+
+    # set edx to the the address of the string to print
+    movl 12(%ebp), %edx
+
+    # call printf()
+    pushl (%edx, %ecx, 4)
+    pushl %ecx
     pushl $template
     call printf
-    addl $4, %esi
+    popl %ecx
+    popl %ecx
 
-    subl $1, %ecx
-    cmpl $0, %ecx
-    jge argloop
+    # increment i
+    incl -4(%ebp)
 
+    # start the next iteration of the loop
+    jmp argloop
+
+argloopend:
+
+    # store the return value in the %eax register
     mov $0, %eax
     leave
     ret
